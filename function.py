82985843c4bd92eed7d3dc611d12eb73a28b10cb
gpaul597 <ngugipaul597@gmail.com>
2022-03-09 12:11:19 +0300

    first commit

---
+++ function.py
@@ -0,0 +1,10 @@
+def student(fname,lname):
+  print(fname, lname)
+
+student(fname='geeks', lname='practice')
+student(lname='practice', fname='geeks')
+
+
+from math import sqrt, factorial
+print (sqrt(16) ) 
+print(factorial(6) )
